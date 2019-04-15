061e2fc2f613119b0b80bea30747bf1c875ffc71
Alex Chen <32882075+achen178@users.noreply.github.com>
2019-04-15 09:40:35 -0400

    Fiddled with images / jscript
    Added parallax effect.
    Scrapped single background image idea for the sake of proper positioning.
    Smooth scroll was broken? Will work on fixing.

--- js/script.js
+++ js/script.js
@@ -1,47 +1,48 @@
-// smooth momentum scroll https://www.jqueryscript.net/animation/Smooth-Momentum-Scrolling-Effect-with-jQuery-Momentum-Scroll.html
-jQuery(function($) {
-
-  "use strict";
-
-  var win = $(window)
-  , target = $('body')
-  , wrapper = target.find('> div')
-  , easing = "cubic-bezier(.3,.59,.4,1.04)" //css easing
-  , duration = "1s" //duration ms(millisecond) or s(second)
-  , top = 0
-  , kineticScroll = {
-    _init: function() {
-      if( wrapper.length == 1 ) {
-        target.height(wrapper.height());
-        wrapper.css({
-          transition: 'transform ' + duration + ' ' + easing,
-          position: 'fixed',
-          top: '0',
-          left: '0',
-          width: '100%',
-          padding: '0',
-          zIndex: '2'
-        });
-
-        win.on({
-          scroll: function () {
-            kineticScroll._scroll();
-          }
-          , resize: function() {
-            target.height(wrapper.outerHeight());
-          }
-        });
-
-        kineticScroll._scroll();
-      }
-    },
-    _scroll: function() {
-      top = win.scrollTop();
-      wrapper.css('transform', 'translateY(-' + top + 'px)');
-    }
-  };
-
-  if (typeof window.ontouchstart == 'undefined') {
-    kineticScroll._init();
-  }
-});
+// smooth momentum sco
+// jQuery(function($) {
+//
+//   "use strict";
+//
+//   var win = $(window)
+//   , target = $('body')
+//   , wrapper = target.find('> div')
+//   , easing = "cubic-bezier(.3,.59,.4,1.04)" //css easing
+//   , duration = "1s" //duration ms(millisecond) or s(second)
+//   , top = 0
+//   , kineticScroll = {
+//     _init: function() {
+//       if( wrapper.length == 1 ) {
+//         target.height(wrapper.height());
+//         wrapper.css({
+//           transition: 'transform ' + duration + ' ' + easing,
+//           position: 'fixed',
+//           top: '0',
+//           left: '0',
+//           width: '100%',
+//           padding: '0',
+//           zIndex: '2'
+//         });
+//
+//         win.on({
+//           scroll: function () {
+//             kineticScroll._scroll();
+//           }
+//           , resize: function() {
+//             target.height(wrapper.outerHeight());
+//           }
+//         });
+//
+//         kineticScroll._scroll();
+//       }
+//     },
+//     _scroll: function() {
+//       top = win.scrollTop();
+//       wrapper.css('transform', 'translateY(-' + top + 'px)');
+//     }
+//   };
+//
+//   if (typeof window.ontouchstart == 'undefined') {
+//     kineticScroll._init();
+//   }
+// });
+$(".my-paroller").paroller();
