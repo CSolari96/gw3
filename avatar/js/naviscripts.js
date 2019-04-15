c024fc51817b44ef3a3ff27bd445fa7536ef756c
craftlifeguru <45809609+craftlifeguru@users.noreply.github.com>
2019-04-15 14:11:04 -0500

    Add files via upload

---
+++ avatar/js/naviscripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$("a[href$=jakebio.png]").lightBox();
+	$("a[href$=neytiribio.png]").lightBox();
+	$("a[href$=moatbio.png]").lightBox();
+  $("a[href$=eytukanbio.png]").lightBox();
+  $("a[href$=tsuteybio.png]").lightBox();
+});
