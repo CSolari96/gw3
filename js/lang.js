ef235fcd847c8ce4a801330e807f07ed9427643c
Courtney Solari <csolari@ufl.edu>
2019-04-15 21:59:32 -0500

    Modified event listeners and added function for type display

--- js/lang.js
+++ js/lang.js
@@ -7,32 +7,62 @@ for (i = 1; i <= 6; i++) {
   navi.push(document.getElementById("lang-translations").children[i]);
 }
 
+var word;
+var speed = 150;
+var x;
+
 navi[0].addEventListener("click", function() {
-    placeholder.innerHTML = translations[0];
+    word = translations[0];         // Define English translation
+    placeholder.innerHTML = "";     // Clear current content of placeholder
+    x = 0;                          // Reset index
+    type();                         // Execute type function
   }, false
 );
 
 navi[1].addEventListener("click", function() {
-    placeholder.innerHTML = translations[1];
+    word = translations[1];         // Define English translation
+    placeholder.innerHTML = "";     // Clear current content of placeholder
+    x = 0;                          // Reset index
+    type();                         // Execute type function
   }, false
 );
 
 navi[2].addEventListener("click", function() {
-    placeholder.innerHTML = translations[2];
+    word = translations[2];         // Define English translation
+    placeholder.innerHTML = "";     // Clear current content of placeholder
+    x = 0;                          // Reset index
+    type();                         // Execute type function
   }, false
 );
 
 navi[3].addEventListener("click", function() {
-    placeholder.innerHTML = translations[3];
+    word = translations[3];         // Define English translation
+    placeholder.innerHTML = "";     // Clear current content of placeholder
+    x = 0;                          // Reset index
+    type();                         // Execute type function
   }, false
 );
 
 navi[4].addEventListener("click", function() {
-    placeholder.innerHTML = translations[4];
+    word = translations[4];         // Define English translation
+    placeholder.innerHTML = "";     // Clear current content of placeholder
+    x = 0;                          // Reset index
+    type();                         // Execute type function
   }, false
 );
 
 navi[5].addEventListener("click", function() {
-    placeholder.innerHTML = translations[5];
+    word = translations[5];         // Define English translation
+    placeholder.innerHTML = "";     // Clear current content of placeholder
+    x = 0;                          // Reset index
+    type();                         // Execute type function
   }, false
 );
+
+function type() {
+  if (x < word.length) {
+    placeholder.innerHTML += word.charAt(x);
+    x++;
+    setTimeout(type, speed);
+  }
+}
